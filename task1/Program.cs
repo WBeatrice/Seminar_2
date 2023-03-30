8bb166fc726e965ac7307bb91d12cfec40d077f3
Beatrice <b.wenedisiuk@gmail.com>
2023-03-30 17:03:03 +0300

    Add task 1

---
+++ task1/Program.cs
@@ -0,0 +1,36 @@
+﻿int number = 0;
+int a = 0;
+int b = 0;
+
+Console.Write("Enter a three-digit number: ");
+
+if (int.TryParse(Console.ReadLine(), out number))
+{
+    if (number >= 100)
+    {
+        if (number % 10 != 0)
+        {
+            a = number / 10;
+
+            if (a % 10 != 0)
+            {
+                b = a % 10;
+                Console.WriteLine($"The second digit of the number: {b}");
+            }
+            else
+            {
+                Console.WriteLine("The second digit of the number: 0");
+            }
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("Wrong number!");
+    }
+
+}
+else
+{
+    Console.WriteLine("Oops");
+}
