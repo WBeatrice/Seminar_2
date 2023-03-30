e1e104963df1297eaf66d8175d8d51a7836dee08
Beatrice <b.wenedisiuk@gmail.com>
2023-03-30 17:06:00 +0300

    Add task 2

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿int number = 0;
+int a = 0;
+
+Console.Write("Enter the number: ");
+if (int.TryParse(Console.ReadLine(), out number))
+{
+    if (number >= 100)
+    {
+        if (number < 1000)
+        {
+            number %= 10;
+            Console.WriteLine($"The third digit of the number: {number}");
+        }
+        else
+        {
+            Console.WriteLine("I can't count");
+        }
+    }
+    else
+    {
+        Console.WriteLine("There is no third number");
+    }
+}
